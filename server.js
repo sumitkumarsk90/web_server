84ba821f9672b40eb079e6c0b863c7a33ef0a39b
Sumit Kumar <sumitkumarsk90@gmail.com>
2016-03-11 01:24:03 +0530

    Update language

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/about', middleware.requireAuthentication, function (req,res) {
 	res.send('It\'s all about us!!!!!');
 });
 
-app.use(express.static(__dirname + "\\public"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(PORT, function () {
 	console.log('Express Server Started at Port number ' + PORT);
