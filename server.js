7c084994305a8e8e589508b7af330e533633a54d
Sumit Kumar <sumitkumarsk90@gmail.com>
2016-03-11 00:13:33 +0530

    Update language

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var middleware = require('./middleware.js');
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req,res) {
-	res.send('It\'s all about us!!!!');
+	res.send('It\'s all about us!!!!!');
 });
 
 app.use(express.static(__dirname + "\\public"));
